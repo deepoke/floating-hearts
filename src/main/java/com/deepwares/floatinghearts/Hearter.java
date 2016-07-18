69d501f235e6b831cb4c69f016b88e0f22ed0b3d
Deepak Sundar <deepak@airtime.com>
2016-07-18 01:03:17 -0400

    Adding more heart options

--- src/main/java/com/deepwares/floatinghearts/Hearter.java
+++ src/main/java/com/deepwares/floatinghearts/Hearter.java
@@ -5,7 +5,6 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.graphics.Interpolator;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,8 +13,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.CycleInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -26,6 +30,10 @@ public class Hearter extends FrameLayout {
     private Heart mSpawner;
     private int mHeartSize;
     private LayoutParams mHeartLayoutParams;
+    private boolean mLeftAligned, mHeartVisible;
+    private int mHeartColor;
+    private String mText;
+
     public Hearter(Context context) {
         super(context);
         init();
@@ -48,59 +56,116 @@ public class Hearter extends FrameLayout {
         super(context, attrs, defStyleAttr, defStyleRes);
         init();
     }
-    private void init(){
-        mHeartSize = 100;
-        setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        setPadding(10, 10, 10, 10);
+
+    public Hearter(Context context, boolean left, boolean heartVisible) {
+        super(context);
+        mHeartVisible = heartVisible;
+        mLeftAligned = left;
+        init();
+    }
+
+    private void init() {
+        mHeartSize = mWidth;
+        //setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        //setPadding(10, 10, 10, 10);
         mSpawner = new Heart(getContext());
         mSpawner.setTextSize(mHeartSize);
         mHeartLayoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        mHeartLayoutParams.gravity= Gravity.BOTTOM|Gravity.RIGHT;
-        setOnClickListener(new OnClickListener() {
+        final Spinner spinner = new Spinner(getContext());
+        spinner.setVisibility(GONE);
+        FrameLayout.LayoutParams choicesLayoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        choicesLayoutParams.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+        choicesLayoutParams.bottomMargin = mHeartSize * 2;
+        addView(spinner, choicesLayoutParams);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(getContext(), getResources().getIdentifier("dropdown", "layout", getContext().getPackageName()));
+        ;
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final List<String> choices = new ArrayList<>();
+        choices.add(getResources().getString(getResources().getIdentifier("heart", "string", getContext().getPackageName())));
+        choices.add(new String(Character.toChars(0x1F44D)));
+        choices.add(new String(Character.toChars(0x1F44F)));
+        choices.add(new String(Character.toChars(0x1F60A)));
+        choices.add(new String(Character.toChars(0x1F620)));
+        mText = choices.get(0);
+        //adapter.clear();
+        adapter.addAll(choices);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                addHeart();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mText = choices.get(position);
+                mSpawner.setText(mText);
+                spinner.setVisibility(GONE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                spinner.setVisibility(GONE);
             }
         });
-        addView(mSpawner, mHeartLayoutParams);
 
+
+        mHeartLayoutParams.gravity = Gravity.BOTTOM | (mLeftAligned ? Gravity.LEFT : Gravity.RIGHT);
+        if (mHeartVisible) {
+
+            mSpawner.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    animateView(getHeart());
+                }
+            });
+            mSpawner.setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    spinner.setVisibility(VISIBLE);
+                    return true;
+                }
+            });
+            mHeartLayoutParams.bottomMargin = 10;
+            mHeartLayoutParams.rightMargin = 10;
+            mHeartLayoutParams.leftMargin = 10;
+
+            addView(mSpawner, mHeartLayoutParams);
+        } else {
+            mSpawner.setVisibility(GONE);
+        }
     }
 
-    private void addHeart(){
-        final Heart heart = new Heart(getContext());
-        heart.setTextSize(mHeartSize);
+    public void animateView(final View view) {
+
         AnimatorSet animatorSet = new AnimatorSet();
         boolean bubble = sRandomizer.nextBoolean();
         ObjectAnimator translateX;
-        if(bubble){
+        if (bubble) {
             boolean mirror = sRandomizer.nextBoolean();
             boolean startLeft = sRandomizer.nextBoolean();
-            int leftX = startLeft ? -sRandomizer.nextInt(200) : sRandomizer.nextInt(200) ;
-            int rightX = mirror? -leftX : - sRandomizer.nextInt(Math.abs(leftX));
-            translateX = ObjectAnimator.ofFloat(heart,TRANSLATION_X,leftX,rightX);
+            int val = sRandomizer.nextInt(200);
+            //int leftX = mLeftAligned ? -sRandomizer.nextInt(200) : sRandomizer.nextInt(200);
+            int rightX = mLeftAligned ? val : -val;
+            translateX = ObjectAnimator.ofFloat(view, TRANSLATION_X, 0, rightX);
             translateX.setInterpolator(new CycleInterpolator(1));
         } else {
-            translateX = ObjectAnimator.ofFloat(heart,TRANSLATION_X,0,0);
+            translateX = ObjectAnimator.ofFloat(view, TRANSLATION_X, 0, 0);
         }
         DecelerateInterpolator scaleInterpolator = new DecelerateInterpolator();
-        ObjectAnimator scaleXInterpolator = ObjectAnimator.ofFloat(heart,SCALE_X,1.4f,1);
+        ObjectAnimator scaleXInterpolator = ObjectAnimator.ofFloat(view, SCALE_X, 1.4f, 1);
         scaleXInterpolator.setInterpolator(scaleInterpolator);
-        ObjectAnimator scaleYInterpolator = ObjectAnimator.ofFloat(heart,SCALE_Y,1.4f,1);
+        ObjectAnimator scaleYInterpolator = ObjectAnimator.ofFloat(view, SCALE_Y, 1.4f, 1);
         scaleYInterpolator.setInterpolator(scaleInterpolator);
-        ObjectAnimator translateY = ObjectAnimator.ofFloat(heart,TRANSLATION_Y,0,-1000f);
-        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(heart, ALPHA, 1, 0);
+        ObjectAnimator translateY = ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, -1000f);
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(view, ALPHA, 1, 0);
         //animatorSet.setInterpolator(new CycleInterpolator(5));
-        addView(heart,mHeartLayoutParams);
+        addView(view,mHeartLayoutParams);
         animatorSet.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
-                Log.d(TAG,"onAnimationStart");
+                Log.d(TAG, "onAnimationStart");
             }
 
             @Override
             public void onAnimationEnd(Animator animator) {
-                Log.d(TAG,"onAnimationEnd");
-                removeView(heart);
+                Log.d(TAG, "onAnimationEnd");
+                removeView(view);
             }
 
             @Override
@@ -113,8 +178,36 @@ public class Hearter extends FrameLayout {
 
             }
         });
-        animatorSet.setDuration(2000);
-        animatorSet.playTogether(translateX,translateY,alphaAnimator,scaleXInterpolator,scaleYInterpolator);
+        animatorSet.setDuration(mDuration);
+        animatorSet.playTogether(translateX, translateY, alphaAnimator, scaleXInterpolator, scaleYInterpolator);
         animatorSet.start();
+    }
+
+    public View getHeart() {
+        final Heart heart = new Heart(getContext());
+        heart.setTextSize(mHeartSize);
+        if (mHeartColor != 0) {
+            heart.setTextColor(mHeartColor);
+        }
+        if (mText != null) {
+            heart.setText(mText);
+        }
+        heart.setLayoutParams(mHeartLayoutParams);
+        return heart;
+    }
+
+    int mWidth = 50, mDuration = 2000;
+
+    public void setWidth(int width) {
+        mWidth = width;
+    }
+
+    public void setDuration(int duration) {
+        mDuration = duration;
+    }
+
+    public void setHeartColor(int color) {
+        mHeartColor = color;
+        mSpawner.setTextColor(color);
     }
 }
